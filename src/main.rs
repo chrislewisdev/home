3f700e162d4480b179721f1dcbabd035adb65658
Chris Lewis-Hou <chris@lewis-hou.dev>
2024-09-21 16:26:17 +1000

    Add very basic md->html generation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::{
     env, fs::{self}, path::{Path, PathBuf}
 };
 
+use anyhow::Context;
+use pulldown_cmark::{html, Parser};
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -30,6 +33,24 @@ fn clean() -> anyhow::Result<()> {
 fn generate() -> anyhow::Result<()> {
     fs::create_dir_all("build/assets")?;
     copy_assets("assets", PathBuf::from("build/assets"))?;
+
+    let layout = fs::read_to_string("content/layout.html")?;
+
+    for entry in fs::read_dir("content")? {
+        let entry = entry?;
+        if entry.path().extension().is_some_and(|v| v == "md") {
+            let md = fs::read_to_string(entry.path())?;
+            let parser = Parser::new(md.as_str());
+
+            let mut htm = String::new();
+            html::push_html(&mut htm, parser);
+            
+            let rendered = layout.clone().replace("{{content}}", &htm);
+            let html_path = entry.path().with_extension("html");
+            let filename = html_path.file_name().context("")?;
+            fs::write(PathBuf::from("build").join(filename), rendered)?;
+        }
+    }
 
     Ok(())
 }
